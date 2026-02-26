08d892676491dcdfd79e86224e20a134c214ab18
pivnati <natanmuleta911@gmail.com>
2026-02-26 07:13:06 -1200

    create the searchbar components folder

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const profile = () => {
+const SearchBar = () => {
   return (
     <View>
-      <Text> profile </Text>
+      <Text>SearchBar</Text>
     </View>
   )
 }
 
-export default profile
+export default SearchBar
 
 const styles = StyleSheet.create({})
