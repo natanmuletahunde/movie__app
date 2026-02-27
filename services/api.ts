e69b6742279311dfb7ec4b76d557735bcbdfd718
pivnati <natanmuleta911@gmail.com>
2026-02-26 23:54:03 -1200

    create the usefetch  in the services folder

--- services/api.ts
+++ services/api.ts
@@ -1,13 +1,54 @@
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzNlZDdjZTAzMWEyOWJkMDEyMzY2ZGVjMTVmNzM3OCIsIm5iZiI6MTc3MjE4NDMzNS43NjMsInN1YiI6IjY5YTE2MzBmYWExOThjZDJkM2UyZGZhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S5E3dNgbefoEJ8RAWzdeJQS00WtcZatlffgGNPvJN38'
-//   }
-// };
+export const TMDB_CONFIG = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    },
+  };
 
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
+  export const fetchMovies = async ({
+    query,
+  }: {
+    query: string;
+  }): Promise<Movie[]> => {
+    const endpoint = query
+      ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+      method: "GET",
+      headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movies: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.results;
+  };
+
+  export const fetchMovieDetails = async (
+    movieId: string
+  ): Promise<MovieDetails> => {
+    try {
+      const response = await fetch(
+        `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+        {
+          method: "GET",
+          headers: TMDB_CONFIG.headers,
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching movie details:", error);
+      throw error;
+    }
+  };
