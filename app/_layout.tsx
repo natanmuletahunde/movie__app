3bfb9201bd11d0bc578a98b368cb5856e857b6e3
pivnati <natanmuleta911@gmail.com>
2026-02-28 08:41:52 -1200

    modify the  movies id and index page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,26 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-      />
+    <>
+      <StatusBar hidden={true} />
 
-      <Stack.Screen
-        name="movie/[id]"
-        options={{ headerShown: false }}
-      />
-
-    </Stack>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
   );
 }
